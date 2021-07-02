dab7bf9b5e2d5585e70407abd35b6a06ec6f9349
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-02 14:20:48 -0500

    feat: add basic content editor interface

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import SplitPane from "react-split-pane";
+import { EditorState } from "draft-js";
+import { Editor as MarkdownEditor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { Card } from "@material-ui/core";
+
+const Wrapper = styled.div`
+  margin: 8px;
+`;
+
+const PreviewContent = styled.div`
+  padding: 16px 8px;
+`;
+
+const PreviewHeading = styled.h2`
+  margin: 0;
+  font-size: 1.125rem;
+`;
 
 function Editor({ newNode = false }: { newNode?: boolean }) {
-  return <div>{newNode ? "true" : "false"}</div>;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (editorState: any) => {
+    setEditorState(editorState);
+  };
+
+  return (
+    <SplitPane split="vertical" minSize="25%" defaultSize="50%">
+      <Wrapper>
+        <MarkdownEditor
+          editorState={editorState}
+          onEditorStateChange={onEditorStateChange}
+        ></MarkdownEditor>
+      </Wrapper>
+      <Wrapper>
+        <Card>
+          <PreviewContent>
+            <PreviewHeading>Content Preview</PreviewHeading>
+            <div>{editorState.getCurrentContent().getPlainText()}</div>
+          </PreviewContent>
+        </Card>
+      </Wrapper>
+    </SplitPane>
+  );
 }
 
 export default Editor;
