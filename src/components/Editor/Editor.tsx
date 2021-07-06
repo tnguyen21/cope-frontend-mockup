9b35e341befa904ac09a4fb6b094005522fb5850
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-06 15:20:03 -0500

    feat: add status and node type to editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import SplitPane from "react-split-pane";
+import { useParams } from "react-router";
 import { EditorState } from "draft-js";
 import { Editor as MarkdownEditor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { makeStyles, Card, Button, TextField } from "@material-ui/core";
+import {
+  makeStyles,
+  Card,
+  FormLabel,
+  Button,
+  FormControl,
+  Select,
+  MenuItem,
+  TextField,
+} from "@material-ui/core";
 
 const Wrapper = styled.div`
-  margin: 16px;
+  margin: 24px 8px;
 `;
 
-const EditorHeading = styled.h3`
-  margin: 8px 0px;
-  font-size: 1.125rem;
+const StyledFormLabel = styled(FormLabel)`
+  margin: 16px 0px 8px;
 `;
 
 const StyledButton = styled(Button)`
@@ -39,33 +48,77 @@ const useStyles = makeStyles({
   },
 });
 
+interface RouteParams {
+  collection?: string;
+  nodeId?: string;
+}
+
 function Editor({ newNode = false }: { newNode?: boolean }) {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const params = useParams<RouteParams>();
   const classes = useStyles();
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [status, setStatus] = useState("DRAFT");
+  const [nodeType, setNodeType] = useState(params.collection?.toUpperCase());
 
   const onEditorStateChange = (editorState: any) => {
     setEditorState(editorState);
   };
 
+  const onStatusChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setStatus(event.target.value as string);
+  };
+
+  const onNodeTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setNodeType(event.target.value as string);
+  };
+
   return (
     <SplitPane split="vertical" minSize="25%" defaultSize="50%">
       <Wrapper>
-        <EditorHeading>Attribute</EditorHeading>
-        <TextField id="standard-basic" label="Example Text Field" />
-        <EditorHeading>Attribute</EditorHeading>
-        <MarkdownEditor
-          editorState={editorState}
-          onEditorStateChange={onEditorStateChange}
-          editorClassName={classes.textEditor}
-        ></MarkdownEditor>
         <Wrapper>
-          <StyledButton variant="contained">Add Parent</StyledButton>
-          <StyledButton variant="contained">Add Sibling</StyledButton>
-          <StyledButton variant="contained">Add Child</StyledButton>
-          <StyledButton variant="contained">Add Asset</StyledButton>
-        </Wrapper>
+          <FormControl>
+            <StyledFormLabel>Node Type</StyledFormLabel>
+            <Select value={nodeType} onChange={onNodeTypeChange} disabled>
+              <MenuItem value="H_AUTHOR">H_AUTHOR</MenuItem>
+              <MenuItem value="H_TEAM">H_TEAM</MenuItem>
+              <MenuItem value="A_ARTICLE">A_ARTICLE</MenuItem>
+              <MenuItem value="A_PAGE">A_PAGE</MenuItem>
+              <MenuItem value="A_APPLICATION">A_APPLICATION</MenuItem>
+              <MenuItem value="A_GEM">A_GEM</MenuItem>
+              <MenuItem value="S_ACS">S_ACS</MenuItem>
+              <MenuItem value="S_CBP">S_CBP</MenuItem>
+              <MenuItem value="V_1990">V_1990</MenuItem>
+              <MenuItem value="V_2000">V_2000</MenuItem>
+              <MenuItem value="V_2010">V_2010</MenuItem>
+              <MenuItem value="V_2020">V_2020</MenuItem>
+              <MenuItem value="C_SERIES">C_SERIES</MenuItem>
+              <MenuItem value="C_LIST">C_LIST</MenuItem>
+            </Select>
 
-        <Button variant="contained">Save Draft</Button>
+            <StyledFormLabel>Status</StyledFormLabel>
+            <Select value={status} onChange={onStatusChange} disabled>
+              <MenuItem value="DRAFT">Draft</MenuItem>
+              <MenuItem value="REVIEWED">Reviewed</MenuItem>
+              <MenuItem value="PUBLISHED">Published</MenuItem>
+              <MenuItem value="EDITED">Edited</MenuItem>
+              <MenuItem value="DELETED">Deleted</MenuItem>
+            </Select>
+
+            <StyledFormLabel>Markdown</StyledFormLabel>
+            <MarkdownEditor
+              editorState={editorState}
+              onEditorStateChange={onEditorStateChange}
+              editorClassName={classes.textEditor}
+            ></MarkdownEditor>
+          </FormControl>
+          <Wrapper>
+            <StyledButton variant="contained">Add Parent</StyledButton>
+            <StyledButton variant="contained">Add Sibling</StyledButton>
+            <StyledButton variant="contained">Add Child</StyledButton>
+            <StyledButton variant="contained">Add Asset</StyledButton>
+          </Wrapper>
+          <Button variant="contained">Save Draft</Button>
+        </Wrapper>
       </Wrapper>
       <Wrapper>
         <Card>
