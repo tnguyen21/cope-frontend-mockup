398c8fc5bf33539ba2ffbfb3dbb82cbd40b2a7a4
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-07 08:27:14 -0500

    misc: modify configs to enable aws user group auth
    due to a strange feature of AWS Cognito + AWS Amplify
    some additional configurations are needed to allow users
    with the 'owner' role to perform operations against
    the live GraphQL API. configs have been added, and
    additionally use `configureWith` from `cope-client-utils`

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,11 @@ import { history } from "./routing/history";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./theme";
 import App from "./components/App";
-import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
-Amplify.configure(awsconfig);
+import { configureWith } from "cope-client-utils";
+// additional configurations in cope-client-utils provided
+// to enable access to GraphQL API
+configureWith(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
