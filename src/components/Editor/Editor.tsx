a1dc6d1fff038a30aadd0490e2d27b6a98c0ce85
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-07 08:42:52 -0500

    misc: add notes and example workflow of createNode
    Added some TODOs as reminders for next steps/nice-to-haves we should
    include in the editor interfaces
    
    Commented out function `createNode` uses  available library
    from `cope-client-utils` to create a node to live GraphQL API
    
    Need to be able to provide correct data from form so that the nodes
    we are creating are actually being created as intended

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,27 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import SplitPane from "react-split-pane";
-import { useParams } from "react-router";
 import { EditorState } from "draft-js";
 import { Editor as MarkdownEditor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import {
   makeStyles,
   Card,
-  FormLabel,
+  InputLabel,
   Button,
-  FormControl,
   Select,
   MenuItem,
-  TextField,
 } from "@material-ui/core";
+import { node } from "cope-client-utils";
+import { NodeType, NodeStatus } from "cope-client-utils/lib/graphql/API";
 
 const Wrapper = styled.div`
   margin: 24px 8px;
-`;
-
-const StyledFormLabel = styled(FormLabel)`
-  margin: 16px 0px 8px;
 `;
 
 const StyledButton = styled(Button)`
@@ -48,82 +43,113 @@ const useStyles = makeStyles({
   },
 });
 
-interface RouteParams {
-  collection?: string;
-  nodeId?: string;
-}
-
+// TODO: useEffect should be called to populate editor with
+// info from an existing node given that newNode is false
 function Editor({ newNode = false }: { newNode?: boolean }) {
-  const params = useParams<RouteParams>();
   const classes = useStyles();
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const [status, setStatus] = useState("DRAFT");
-  const [nodeType, setNodeType] = useState(params.collection?.toUpperCase());
+  const [nodeStatus, setStatus] = useState(NodeStatus);
+  const [nodeType, setNodeType] = useState(NodeType);
 
   const onEditorStateChange = (editorState: any) => {
     setEditorState(editorState);
   };
 
-  const onStatusChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setStatus(event.target.value as string);
+  const onStatusChange = (event: React.ChangeEvent<{ value: any }>) => {
+    setStatus(event.target.value);
   };
 
-  const onNodeTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setNodeType(event.target.value as string);
+  const onNodeTypeChange = (event: React.ChangeEvent<{ value: any }>) => {
+    setNodeType(event.target.value);
   };
+
+  // const createNode = () => {
+  //   let data = {
+  //     id: null,
+  //     status: NodeStatus.DRAFT,
+  //     type: NodeType.A_GEM,
+  //     createdAt: null,
+  //     owner: null,
+  //     updatedAt: null,
+  //   };
+  //   node
+  //     .create(data)
+  //     .then((result) => {
+  //       console.log(result);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
 
   return (
     <SplitPane split="vertical" minSize="25%" defaultSize="50%">
       <Wrapper>
         <Wrapper>
-          <FormControl>
-            <StyledFormLabel>Node Type</StyledFormLabel>
-            <Select value={nodeType} onChange={onNodeTypeChange} disabled>
-              <MenuItem value="H_AUTHOR">H_AUTHOR</MenuItem>
-              <MenuItem value="H_TEAM">H_TEAM</MenuItem>
-              <MenuItem value="A_ARTICLE">A_ARTICLE</MenuItem>
-              <MenuItem value="A_PAGE">A_PAGE</MenuItem>
-              <MenuItem value="A_APPLICATION">A_APPLICATION</MenuItem>
-              <MenuItem value="A_GEM">A_GEM</MenuItem>
-              <MenuItem value="S_ACS">S_ACS</MenuItem>
-              <MenuItem value="S_CBP">S_CBP</MenuItem>
-              <MenuItem value="V_1990">V_1990</MenuItem>
-              <MenuItem value="V_2000">V_2000</MenuItem>
-              <MenuItem value="V_2010">V_2010</MenuItem>
-              <MenuItem value="V_2020">V_2020</MenuItem>
-              <MenuItem value="C_SERIES">C_SERIES</MenuItem>
-              <MenuItem value="C_LIST">C_LIST</MenuItem>
+          {/* These are hardcoded -- should come up with a way to create these based on enums from GraphQL */}
+          <Wrapper>
+            <InputLabel>Node Type</InputLabel>
+            <Select value={nodeType} onChange={onNodeTypeChange}>
+              <MenuItem value={NodeType.H_AUTHOR}>H_AUTHOR</MenuItem>
+              <MenuItem value={NodeType.H_TEAM}>H_TEAM</MenuItem>
+              <MenuItem value={NodeType.A_ARTICLE}>A_ARTICLE</MenuItem>
+              <MenuItem value={NodeType.A_PAGE}>A_PAGE</MenuItem>
+              <MenuItem value={NodeType.A_APPLICATION}>A_APPLICATION</MenuItem>
+              <MenuItem value={NodeType.A_GEM}>A_GEM</MenuItem>
+              <MenuItem value={NodeType.S_ACS}>S_ACS</MenuItem>
+              <MenuItem value={NodeType.S_CBP}>S_CBP</MenuItem>
+              <MenuItem value={NodeType.V_1990}>V_1990</MenuItem>
+              <MenuItem value={NodeType.V_2000}>V_2000</MenuItem>
+              <MenuItem value={NodeType.V_2010}>V_2010</MenuItem>
+              <MenuItem value={NodeType.V_2020}>V_2020</MenuItem>
+              <MenuItem value={NodeType.C_SERIES}>C_SERIES</MenuItem>
+              <MenuItem value={NodeType.C_LIST}>C_LIST</MenuItem>
             </Select>
+          </Wrapper>
 
-            <StyledFormLabel>Status</StyledFormLabel>
-            <Select value={status} onChange={onStatusChange} disabled>
-              <MenuItem value="DRAFT">Draft</MenuItem>
-              <MenuItem value="REVIEWED">Reviewed</MenuItem>
-              <MenuItem value="PUBLISHED">Published</MenuItem>
-              <MenuItem value="EDITED">Edited</MenuItem>
-              <MenuItem value="DELETED">Deleted</MenuItem>
+          <Wrapper>
+            <InputLabel>Status</InputLabel>
+            <Select value={nodeStatus} onChange={onStatusChange}>
+              <MenuItem value={NodeStatus.DRAFT}>Draft</MenuItem>
+              <MenuItem value={NodeStatus.REVIEWED}>Reviewed</MenuItem>
+              <MenuItem value={NodeStatus.PUBLISHED}>Published</MenuItem>
+              <MenuItem value={NodeStatus.EDITED}>Edited</MenuItem>
+              <MenuItem value={NodeStatus.DELETED}>Deleted</MenuItem>
             </Select>
+          </Wrapper>
 
-            <StyledFormLabel>Markdown</StyledFormLabel>
+          <Wrapper>
+            <InputLabel>Markdown</InputLabel>
             <MarkdownEditor
               editorState={editorState}
               onEditorStateChange={onEditorStateChange}
               editorClassName={classes.textEditor}
             ></MarkdownEditor>
-          </FormControl>
+          </Wrapper>
+
           <Wrapper>
             <StyledButton variant="contained">Add Parent</StyledButton>
             <StyledButton variant="contained">Add Sibling</StyledButton>
             <StyledButton variant="contained">Add Child</StyledButton>
             <StyledButton variant="contained">Add Asset</StyledButton>
           </Wrapper>
-          <Button variant="contained">Save Draft</Button>
+
+          <Button
+            variant="contained"
+            onClick={() => console.log("Save Draft was clicked")}
+          >
+            Save Draft
+          </Button>
         </Wrapper>
       </Wrapper>
       <Wrapper>
         <Card>
           <PreviewContent>
             <PreviewHeading>Content Preview</PreviewHeading>
+            {/* TODO: look into `remark.js` and `draft.js` APIs to 
+                be able to parse out content from editor state and 
+                convert to necessary HTML/Markdown to create content preview
+            */}
             <div>{editorState.getCurrentContent().getPlainText()}</div>
           </PreviewContent>
         </Card>
