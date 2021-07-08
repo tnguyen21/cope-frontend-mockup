00e5ef6cc3ed5e5bd8e01e2eaa4361749124bb54
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-08 13:10:18 -0500

    misc: add utils with helpful constants
    typescript Enums aren't easily iterable in the ways that objects or
    arrays are. just putting NodeTypes and NodeStatuses as an object
    as they've been helpful during development, and will look for a more
    elegant and general solution later

---
+++ src/components/Editor/utils.js
@@ -0,0 +1,52 @@
+import {
+  NodeStatus,
+  NodeType,
+  AssetType,
+} from "cope-client-utils/lib/graphql/API";
+
+export const NODE_STATUSES = {
+  DRAFT: NodeStatus.DRAFT,
+  REVIEWED: NodeStatus.REVIEWED,
+  PUBLISHED: NodeStatus.PUBLISHED,
+  EDITED: NodeStatus.EDITED,
+  DELETED: NodeStatus.DELETED,
+};
+
+export const NODE_TYPES = {
+  H_AUTHOR: NodeType.H_AUTHOR,
+  H_TEAM: NodeType.H_TEAM,
+  A_ARTICLE: NodeType.A_ARTICLE,
+  A_PAGE: NodeType.A_PAGE,
+  A_APPLICATION: NodeType.A_APPLICATION,
+  A_GEM: NodeType.A_GEM,
+  S_ACS: NodeType.S_ACS,
+  S_CBP: NodeType.S_CBP,
+  V_1990: NodeType.V_1990,
+  V_2000: NodeType.V_2000,
+  V_2010: NodeType.V_2010,
+  V_2020: NodeType.V_2020,
+  C_SERIES: NodeType.C_SERIES,
+  C_LIST: NodeType.C_LIST,
+};
+
+export const ASSET_TYPES = {
+  A_IMAGE: AssetType.A_IMAGE,
+  A_OG_IMAGE: AssetType.A_OG_IMAGE,
+  A_OG_AUDIO: AssetType.A_OG_AUDIO,
+  A_OG_VIDEO: AssetType.A_OG_VIDEO,
+  A_VIDEO: AssetType.A_VIDEO,
+  A_AUDIO: AssetType.A_AUDIO,
+  T_OG_TITLE: AssetType.T_OG_TITLE,
+  T_OG_DESCRIPTION: AssetType.T_OG_DESCRIPTION,
+  T_OG_TYPE: AssetType.T_OG_TYPE,
+  T_LEDE: AssetType.T_LEDE,
+  T_BODY: AssetType.T_BODY,
+  M_DATA: AssetType.M_DATA,
+  M_MAP: AssetType.M_MAP,
+  M_VIZ: AssetType.M_VIZ,
+  M_API: AssetType.M_API,
+  F_PDF: AssetType.F_PDF,
+  F_KML: AssetType.F_KML,
+  F_SHP: AssetType.F_SHP,
+  F_CSV: AssetType.F_CSV,
+};
